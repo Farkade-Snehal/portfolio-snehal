e4f4edef0532e0d40e7a71ee28181d5cab846960
Snehal Farkade <snehalfarkade23@gmail.com>
2026-02-18 20:14:01 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import path from "path"
 //
 export default defineConfig({
   plugins: [react(), tailwindcss() ],
+  base:process.env.VITE_BASE_PATH || "/portfolio-snehal"
   resolve:{
     alias:{
       "@":path.resolve(__dirname , "./src"),
